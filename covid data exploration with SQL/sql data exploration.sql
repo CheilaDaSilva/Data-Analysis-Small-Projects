5814b29421b9e1c59eb49677962b54b3437d6c63
Cheila Da Silva <88495091+CheilaDaSilva@users.noreply.github.com>
2022-12-23 16:03:18 +0000

    Update sql data exploration.sql

--- covid data exploration with SQL/sql data exploration.sql
+++ covid data exploration with SQL/sql data exploration.sql
@@ -35,6 +35,7 @@ ORDER BY 1,2
 
 -- Total Cases vs Total Deaths
 -- likelihood of dying if you contract covid in the UK over time
+
 SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 as PercentageCasesDeceased
 FROM CovidDeaths
 WHERE location like '%united kingdom%' and continent IS NOT NULL
@@ -43,6 +44,7 @@ ORDER BY 1,2
 
 -- Total Cases vs Population
 -- Percentage of population infected with Covid over time
+
 SELECT location, date, population, total_cases, (total_cases/population)*100 as PercentPopulationInfected
 FROM CovidDeaths
 WHERE continent IS NOT NULL
@@ -51,6 +53,7 @@ ORDER BY 1,2
 
 
 -- Looking at countries with the Highest Infection Rates based on population size
+
 SELECT location, population, MAX(total_cases) AS HighestInfectionCount, (max(total_cases)/population)*100 as PercentPopulationInfected
 FROM CovidDeaths
 WHERE continent is not null
@@ -60,6 +63,7 @@ ORDER BY PercentPopulationInfected desc
 
 
 -- Looking at Countries with Highest Death Count
+
 SELECT location, MAX(CAST(total_deaths AS int)) as TotalDeathCount
 FROM CovidDeaths
 WHERE continent is not null
@@ -75,23 +79,18 @@ ORDER BY TotalDeathCount desc
 
 
 -- Looking at the continents with the highest death count
+
 SELECT continent, MAX(cast(total_deaths as int)) as TotalDeathCount
 FROM CovidDeaths
 WHERE continent is not null
 GROUP BY continent
 ORDER BY TotalDeathCount desc
 
--- getting highest death counts by continent using the location variable
-SELECT location, MAX(cast(total_deaths as int)) as TotalDeathCount
-FROM CovidDeaths
-WHERE continent is null AND location NOT LIKE '%income%'
-GROUP BY location
-ORDER BY TotalDeathCount desc
-
 
 -- GLOBAL NUMBERS
 
 -- worldwide death percentage
+
 SELECT SUM(new_cases) as total_cases
 , SUM(CAST(new_deaths as int)) as total_deaths 
 ,(SUM(CAST(new_deaths as int))/SUM(new_cases))*100 as PercentageCasesDeceased
@@ -101,6 +100,7 @@ ORDER BY 1,2
 
 
 -- join our two tables
+
 SELECT * 
 FROM CovidDeaths dea
 JOIN CovidVaccinations vac
@@ -111,16 +111,6 @@ JOIN CovidVaccinations vac
 -- Total Population vs Vaccinations
 -- Looking at percentage of population that has received at least one covid vaccination
 
-SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations 
-FROM CovidDeaths dea
-JOIN CovidVaccinations vac
-	ON dea.location = vac.location
-	AND dea.date = vac.date
-WHERE dea.continent is not null
-ORDER BY 2,3
-
-
--- using over partition to aggregate sum by location
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
 , SUM(convert(int, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) as RollingPeopleVaccinated
 FROM CovidDeaths dea
@@ -132,6 +122,7 @@ ORDER BY 2,3
 
 
 -- Using CTEs to use calculated columns with partition by
+
 
 WITH PopvsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
 AS
@@ -147,6 +138,7 @@ WHERE dea.continent is not null
 
 SELECT * , (RollingPeopleVaccinated/Population)*100
 FROM PopvsVac;
+
 
 -- Using TEMP TABLES to use calculated columns with partition by
 
@@ -188,3 +180,4 @@ WHERE dea.continent is not null
 
 SELECT * 
 FROM PercentPopulationVaccinated
+
